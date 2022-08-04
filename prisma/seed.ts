c5ec993d2428fd071422abbcf2db78b5b06f6ac0
DOGHOUSETV <arubigeorge0@gmail.com>
2022-08-04 10:26:19 -0400

    Create seed data

---
+++ prisma/seed.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client';
+import data from '../data/links';
+const prisma = new PrismaClient();
+
+const main = async () => {
+  await prisma.user.create({
+    data: {
+      email: `testemail@gmail.com`,
+      role: 'ADMIN',
+    },
+  })
+
+  await prisma.link.createMany({
+    data: data,
+  })
+}
+
+main()
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
