28f3e13c44339caedf8208486eb51e727a60ff46
DOGHOUSETV <arubigeorge0@gmail.com>
2022-08-09 08:52:54 -0400

    Additional comments

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
+// Learn more: https://pris.ly/d/help/next-js-best-practices
 
 let prisma: PrismaClient;
 
@@ -13,5 +14,4 @@ if (process.env.NODE_ENV === 'production') {
   }
   prisma = global.prisma;
 }
-
 export default prisma;
