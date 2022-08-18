a18395ef4ff706df3c62064732c695c770e1fe38
DOGHOUSETV <arubigeorge0@gmail.com>
2022-08-18 14:05:48 -0400

    Initialize Nexus object types

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,8 +1,9 @@
 import { makeSchema } from 'nexus';
 import { join } from 'path';
+import * as types from './types';
 
 const schema = makeSchema({
-  types: [],
+  types,
   outputs: {
     typegen: join(process.cwd(), 'node_modules', '@types', 'nexus-typegen', 'index.d.ts'),
     schema: join(process.cwd(), 'graphql', 'schema.graphql'),
