8d8776da3885f357b4619dd82e59f9df9e0dad84
DOGHOUSETV <arubigeorge0@gmail.com>
2022-09-29 01:42:44 -0400

    Return paginated data from the database

--- graphql/types/Link.ts
+++ graphql/types/Link.ts
@@ -63,19 +63,62 @@ export const LinksQuery = extendType({
         after: stringArg(),
       },
       async resolve(_, args, ctx) {
-        return {
-          edges: [
-            {
-              cursor: '',
-              node: {
-                title: '',
-                category: '',
-                imageUrl: '',
-                url: '',
-                description: '',
-              },
+        let queryResults = null
+
+        if (args.after) {
+          // Check if there is a cursor as the argument
+          queryResults = await ctx.prisma.link.findMany({
+            take: args.first, // the number of items to return from the database
+            skip: 1, // skip the cursor
+            cursor: {
+              id: args.after, // the cursor
             },
-          ],
+          })
+        } else {
+          // If no cursor, this means that this is the first request
+          // and we will return the first items in the database
+          queryResults = await ctx.prisma.link.findMany({
+            take: args.first,
+          })
+        }
+        // If the initial request returns links
+        if (queryResults.length > 0) {
+          // get last element in previous result set
+          const lastLinkInResults = queryResults[queryResults.length - 1]
+          // cursor we'll return in subsequent requests
+          const myCursor = lastLinkInResults.id
+
+          // Query after the cursor to check if we have nextPage
+          const secondQueryResults = await ctx.prisma.link.findMany({
+            take: args.first,
+            cursor: {
+              id: myCursor,
+            },
+            orderBy: {
+              index: 'asc',
+            },
+          })
+          // Return response
+          const result = {
+            pageInfo: {
+              endCursor: myCursor,
+              hasNextPage: secondQueryResults.length >= args.first, // If the number of items requested is greater than the response of the second query, we have another page
+            },
+            edges: queryResults.map(link => ({
+              cursor: link.id,
+              node: link,
+            })),
+          }
+
+          return result
+        }
+        //
+        return {
+          pageInfo: {
+            endCursor: null,
+            hasNextPage: false,
+          },
+          edges: [],
         }
       },
     })
