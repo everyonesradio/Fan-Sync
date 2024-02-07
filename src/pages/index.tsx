ec0f28bba0c2453279b4de3a8bcf63abb968cb80
DOGHOUSETV <arubigeorge0@gmail.com>
2024-02-07 15:42:57 -0500

    Clean up code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,7 @@
 import Head from 'next/head'
 import { Inter } from "next/font/google"
-// import License from '@/components/License'
-import Test from '@/components/test'
-import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
-
-const pexel = (id: number) => `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`
-const images: any = [
-  // Front
-  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: pexel(1103970) },
-  // Back
-  // { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430) },
-  // { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452) },
-  // // Left
-  // { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: pexel(327482) },
-  // { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: pexel(325185) },
-  // { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: pexel(358574) },
-  // // Right
-  // { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: pexel(227675) },
-  // { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: pexel(911738) },
-  // { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: pexel(1738986) }
-]
 
 const Home = () => {
   return (
@@ -32,10 +12,11 @@ const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div>
-          <h1 className="text-9xl font-sans">Fan License</h1>
-          <Test images={images}/>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            Fan License
+          </p>
         </div>
       </main>
     </>
