718332b52012e27598d2c8191d96fa886d46a207
DOGHOUSETV <arubigeorge0@gmail.com>
2024-02-14 22:21:04 -0500

    Add ThemeProvider and @react95/icons CSS file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import '@/styles/globals.css';
+import '@react95/icons/icons.css';
 import type { AppProps } from 'next/app';
+import { ThemeProvider, GlobalStyle } from '@react95/core';
 import { LicenseProvider } from '@/components/context/LicenseContext';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <LicenseProvider>
-      <Component {...pageProps} />
-    </LicenseProvider>
+    <ThemeProvider theme={'blackAndWhite'}>
+      <GlobalStyle />
+      <LicenseProvider>
+        <Component {...pageProps} />
+      </LicenseProvider>
+    </ThemeProvider>
   );
-
 }
