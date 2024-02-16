5e397bb9a70be7ea59f0d375f0df17fb92ff0ae5
DOGHOUSETV <arubigeorge0@gmail.com>
2024-02-15 23:50:02 -0500

    Custom tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      scrollbar: {
+        hide: 'scrollbar-hide',
+      },
+    },
   },
   plugins: [],
 }
