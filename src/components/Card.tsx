43ff3dbb7e39a3c6a2887fd8e3a87958461add05
DOGHOUSETV <arubigeorge0@gmail.com>
2024-04-05 23:12:19 -0400

    License card carousel

---
+++ src/components/Card.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Image from "next/image";
+
+interface CardProps {
+  card: string;
+};
+
+const Card: React.FC<CardProps> = ({ card }) => {
+  return (
+    <div className="relative overflow-hidden justify-center items-center min-h-[200px] min-w-[200px] md:min-h-[300px] md:min-w-[300px] lg:min-h-[400px] lg:min-w-[200px]">
+      <Image src={card} alt="Card Image" fill style={{objectFit: "contain"}} />
+    </div>
+  );
+};
+
+export default Card;
--- src/views/CardSection.tsx
+++ src/views/CardSection.tsx
@@ -1,15 +1,56 @@
-import React from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { animate, motion, useMotionValue } from "framer-motion";
+import Card from"@/components/Card";
+import useMeasure from "react-use-measure";
 import { Button } from '@react95/core';
-import { Access229 } from '@react95/icons';
 
 const CardSection = ({ isConnected }: any)  => {
   const router = useRouter();
+  const cards = [
+    "/images/card-1.png",
+    "/images/card-2.png",
+    "/images/card-3.png",
+    "/images/card-4.png",
+    "/images/card-5.png",
+    "/images/card-6.png",
+  ];
+
+  let [ref, {width}] = useMeasure();
+  const xTranslation = useMotionValue(0);
+
+  useEffect(() => {
+    let controls
+    let finalPosition = -width / 2 - 8;
+
+    controls = animate(xTranslation, [0, finalPosition], {
+      ease: "linear",
+      duration: 15,
+      repeat: Infinity,
+      repeatType: "loop",
+      repeatDelay: 0,
+    });
+
+    return () => {
+      if (controls) {
+        controls.stop();
+      }
+   };
+  }, [width, xTranslation]);
 
   return (
     <div  className="snap-start bg-green-200 w-screen h-screen flex items-center justify-center text-8xl">
-      <div className="flex flex-col items-center justify-center space-y-4 ">
-        <h1 className="font-bold p-8">Card Section</h1>
+      <div className="flex flex-col items-center justify-center space-y-4">
+        <h3 className="font-bold p-8">Join the Seddy Siren Club</h3>
+        <motion.div 
+          className="left-0 flex gap-12" 
+          ref={ref}
+          style={{ x: xTranslation }}
+        >
+          {[...cards, ...cards].map((item, idx) => (
+            <Card card={item} key={idx}/>
+          ))}
+        </motion.div>
         <div className="flex flex-col items-center justify-center space-y-2">
           {isConnected ? (
             <h2 className="text-xl font-bold text-center">
@@ -20,12 +61,11 @@ const CardSection = ({ isConnected }: any)  => {
               Could not connect to MongoDB
             </h2>
           )}
-          <Access229 />
           <Button onClick={() => router.push('/upload')}>Create Your License</Button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default CardSection;
