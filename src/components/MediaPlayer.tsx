ab7f5db58b54da20e3c0aca60a7eef99f01bfdb0
DOGHOUSETV <arubigeorge0@gmail.com>
2024-04-05 23:09:54 -0400

    Spotify anthem player

---
+++ src/components/MediaPlayer.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+import { Album } from "@/types/catalog";
+
+type MediaPlayerProps = {
+ selectedAnthem: Album;
+};
+
+const upperCase = (string: string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+const MediaPlayer: React.FC<MediaPlayerProps> = ({ selectedAnthem }) => {
+ return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg m-4 bg-white">
+      <Image className="w-full" src={selectedAnthem.images[0].url} alt="Album Cover" width={100} height={100} />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{selectedAnthem.name}</div>
+        <p className="text-gray-700 text-base">
+          {upperCase(selectedAnthem.album_type)} - {selectedAnthem.release_date.split('-')[0]}
+        </p>
+      </div>
+    </div>
+ );
+};
+
+export default MediaPlayer;
