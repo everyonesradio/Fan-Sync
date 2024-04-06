3efec4ff13bc213770a6defc668ba50f27acb4e4
DOGHOUSETV <arubigeorge0@gmail.com>
2024-04-05 23:15:05 -0400

    License page

---
+++ src/pages/license.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const License = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-5xl text-center p-8">Share Your License</h1>
+    </div>
+  );
+};
+
+export default License;
