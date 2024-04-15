514d7ad2eac258708bb69607be6c873be7b6288f
DOGHOUSETV <arubigeorge0@gmail.com>
2024-04-15 12:45:29 -0400

    Update catalog types

--- src/types/catalog.ts
+++ src/types/catalog.ts
@@ -1,11 +1,20 @@
-export type Album = {
+export type Artist = {
   id: string;
   name: string;
+ };
+ 
+ export type Catalog = {
+  id: string;
+  name: string;
+  preview_url: string;
+  track_url: string;
+  artists: Artist[];
   images: { url: string }[];
   album_type: string;
+  album_group: string;
   release_date: string;
-};
-
-export type ArtistCatalog = {
-  items: Album[];
-};
+ };
+ 
+ export type ArtistCatalog = {
+  items: Catalog[];
+ };
