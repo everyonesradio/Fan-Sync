7b2f13b745d1fc31fdea2d6a9d6ad33ed9fe231d
DOGHOUSETV <arubigeorge0@gmail.com>
2024-10-03 15:11:23 -0400

    Add demo button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 // ** React/Next.js Imports
 import Image from "next/image";
+import { useRouter } from "next/router";
 import React, { useState, useEffect } from "react";
 
 // ** Third-Party Imports
@@ -9,10 +10,11 @@ import { ToastContainer, toast } from "react-toastify";
 import { api } from "@/utils/trpc";
 
 const Home = () => {
-  const { mutateAsync: waitlistEntry } = api.waitlist.add.useMutation();
-
+  const router = useRouter();
   const [email, setEmail] = useState<string>();
   const [errorMessage, setErrorMessage] = useState<string>();
+
+  const { mutateAsync: waitlistEntry } = api.waitlist.add.useMutation();
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
@@ -22,21 +24,21 @@ const Home = () => {
     e.preventDefault();
 
     // Validation checks
-    const emailRegex = /^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$/;
+    const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
 
     if (!email || !emailRegex.test(email)) {
       setErrorMessage("Please enter a valid email address!");
       return;
     }
-    
+
     if (email) {
       try {
         const res = await waitlistEntry({
           email: email,
         });
 
-        setEmail('');
-        setErrorMessage('');
+        setEmail("");
+        setErrorMessage("");
 
         toast.success(res.message, {
           position: "bottom-right",
@@ -74,63 +76,77 @@ const Home = () => {
   }, [errorMessage]);
 
   return (
-    <main className='flex min-h-screen bg-black p-16'>
-      <div className='flex flex-col w-1/2'>
-        <div className='flex items-center mb-10 space-x-4'>
+    <main className='min-h-screen bg-black p-8 px-16'>
+      <div className='flex items-center justify-between mb-10'>
+        <div className='flex items-center space-x-4'>
           <div className='w-10 h-10 rounded-full bg-white'></div>
           <div className='w-96 h-10 bg-white rounded-full'></div>
           <div className='w-48 h-10 bg-black border-4 border-white rounded-full'></div>
         </div>
-        <h1 className='text-white text-9xl font-bold mb-8'>FanSync</h1>
-        <div className='text-white text-3xl font-medium mt-6'>
-          <p>A platform where you can take full</p>
-          <p>control of your fanbase</p>
-        </div>
-        <form onSubmit={handleSubmit} method='POST' className='mt-32 max-w-sm'>
-          <div className='flex flex-col gap-2 lg:flex-row'>
-            <label className='sr-only' htmlFor='email-address'>
-              Email address
-            </label>
-            <input
-              className='text-accent-500 block h-10 w-full appearance-none px-4 py-2 placeholder-zinc-400 duration-200 focus:outline-none focus:ring-zinc-300 sm:text-sm'
-              id='email-address'
-              name='email'
-              placeholder='johndoe@example.com'
-              value={email}
-              onChange={handleEmailChange}
-            />
-            <button
-              className='flex h-10 shrink-0 items-center justify-center gap-2 border-2 border-[#E5E900] px-4 py-2 text-sm font-semibold text-white transition-all hover:bg-[#E5E900] hover:text-black'
-              type='submit'
-            >
-              <span>Join the waitlist</span>
-            </button>
-          </div>
-        </form>
+        <button
+          onClick={() => router.push("/sgawd")}
+          className='flex items-center justify-center h-10 bg-[#DAE100] p-6 text-xl font-bold text-black transition-all hover:bg-[#E5E900]'
+        >
+          <span>DEMO</span>
+        </button>
       </div>
-      <div className='w-1/2 flex items-center justify-center'>
-        <Image
-          src='/images/home-page/image.png'
-          alt='FanSync Home Image'
-          width={650}
-          height={650}
-          style={{
-            objectFit: "contain",
-          }}
-        />
+      <div className='flex'>
+        <div className='flex flex-col w-1/2'>
+          <h1 className='text-white text-9xl font-bold mb-8'>FanSync</h1>
+          <div className='text-white text-3xl font-medium mt-6'>
+            <p>A platform where you can take full</p>
+            <p>control of your fanbase</p>
+          </div>
+          <form
+            onSubmit={handleSubmit}
+            method='POST'
+            className='mt-32 max-w-sm'
+          >
+            <div className='flex flex-col gap-2 lg:flex-row'>
+              <label className='sr-only' htmlFor='email-address'>
+                Email address
+              </label>
+              <input
+                className='text-accent-500 block h-10 w-full appearance-none px-4 py-2 placeholder-zinc-400 duration-200 focus:outline-none focus:ring-zinc-300 sm:text-sm'
+                id='email-address'
+                name='email'
+                placeholder='johndoe@example.com'
+                value={email}
+                onChange={handleEmailChange}
+              />
+              <button
+                className='flex h-10 shrink-0 items-center justify-center gap-2 border-2 border-[#DAE100] px-4 py-2 text-sm font-semibold text-white transition-all hover:bg-[#DAE100] hover:text-black'
+                type='submit'
+              >
+                <span>Join the waitlist</span>
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className='w-1/2 flex items-center justify-center'>
+          <Image
+            src='/images/home-page/image.png'
+            alt='FanSync Home Image'
+            width={650}
+            height={650}
+            style={{
+              objectFit: "contain",
+            }}
+          />
+        </div>
       </div>
       <ToastContainer
-          position='bottom-right'
-          autoClose={1800}
-          hideProgressBar
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable={false}
-          pauseOnHover
-          theme='colored'
-        />
+        position='bottom-right'
+        autoClose={1800}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable={false}
+        pauseOnHover
+        theme='colored'
+      />
     </main>
   );
 };
