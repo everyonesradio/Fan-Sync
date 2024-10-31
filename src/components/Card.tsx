61dc7e0691f2561ff5bfdcac091d2d856345e1ee
DOGHOUSETV <arubigeorge0@gmail.com>
2024-10-30 23:07:42 -0400

    Resize cards for mobile screens

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ card }) => {
   return (
-    <div className='relative overflow-hidden justify-center items-center min-h-[200px] min-w-[200px] md:min-h-[300px] md:min-w-[300px] lg:min-h-[400px] lg:min-w-[200px]'>
+    <div className='relative overflow-hidden justify-center items-center min-h-[400px] min-w-[200px] md:min-h-[300px] md:min-w-[300px] lg:min-h-[400px] lg:min-w-[200px]'>
       <Image
         src={card}
         alt='Card Image'
