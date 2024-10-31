a759d7ca7f8bc44f912621b8b93c91cf28887797
DOGHOUSETV <arubigeorge0@gmail.com>
2024-10-30 23:08:00 -0400

    More responsive design stuff

--- src/views/AboutSection.tsx
+++ src/views/AboutSection.tsx
@@ -4,11 +4,11 @@ import React from "react";
 const AboutSection = () => {
   return (
     <div className='snap-start bg-black w-screen h-screen flex items-center justify-center'>
-      <div className='flex flex-col w-full sm:m-16 sm:p-12 space-y-8'>
+      <div className='flex flex-col w-full m-8 p-4 sm:m-16 sm:p-12 space-y-8'>
         <h1 className='text-2xl sm:text-6xl font-bold text-white'>
           ...a trailblazing rap sensation on the rise
         </h1>
-        <div className='text-white text-md sm:text-xl space-y-6'>
+        <div className='flex flex-col text-white text-md sm:text-xl space-y-4'>
           <span>
             SGaWD has become a household name and a force to be reckoned with.
             The female MC emerges at the forefront of a new generation of
@@ -25,7 +25,31 @@ const AboutSection = () => {
             freedom.
           </span>
         </div>
-        <div className='sm:flex items-center justify-center gap-12 hidden'>
+        <div className='flex-row space-y-4 sm:hidden w-full items-center justify-center sm:space-x-12'>
+          {/* YouTube Video Embed */}
+          <div>
+            <iframe
+              src='https://www.youtube.com/embed/c84st3J2znQ?si=Jlind0gUS46WUH24'
+              title='YouTube Video Player'
+              allowFullScreen
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+              referrerPolicy='strict-origin-when-cross-origin'
+              loading='lazy'
+            ></iframe>
+          </div>
+          {/* Spotify Embed */}
+          <div>
+            <iframe
+              title='Spotify Album Embed'
+              src='https://open.spotify.com/embed/album/72FBV70oygMH6ELBZ4Zod6?utm_source=generator'
+              width='100%'
+              allowFullScreen
+              allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
+              loading='lazy'
+            ></iframe>
+          </div>
+          </div>
+        <div className='hidden space-y-4 sm:flex w-full items-center justify-center sm:space-x-12'>
           {/* YouTube Video Embed */}
           <div>
             <iframe
