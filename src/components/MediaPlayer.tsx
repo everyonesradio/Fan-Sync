dd11976cef26543749f4e3715570f982db78f8a5
DOGHOUSETV <40546199+GeorgeArubi@users.noreply.github.com>
2024-11-07 09:14:29 -0500

    fix: standardize album cover dimensions for consistent sizing and aspect ratio

--- src/components/MediaPlayer.tsx
+++ src/components/MediaPlayer.tsx
@@ -39,13 +39,16 @@ const MediaPlayer: React.FC<MediaPlayerProps> = ({ selectedAnthem }) => {
 
   return (
     <div className='max-w-sm rounded overflow-hidden shadow-lg m-4 bg-white'>
-      <Image
+      <div className='relative w-full h-48'>
+<Image
         className='w-full h-full object-cover'
         src={selectedAnthem.images[0].url}
         alt='Album Cover'
-        width={100}
-        height={100}
+        width={200}
+        height={200}
+layout="fill"
       />
+</div>
       <div className='px-6 py-4'>
         <div className='font-bold text-xl mb-2'>{selectedAnthem.name}</div>
         <p className='text-gray-700 text-base'>
