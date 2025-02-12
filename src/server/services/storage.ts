c9b1167636d205c262f713fa29e7603ed8a2ef5c
DOGHOUSETV <arubigeorge0@gmail.com>
2025-02-12 16:15:47 -0500

    create firebase image storage service

--- src/server/services/storage.ts
+++ src/server/services/storage.ts
@@ -1,1 +1,25 @@
-// TODO: Move storage api to trpc route
+// ** Third-Party Imports
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
+// ** Custom Components, Hooks, Utils, etc.
+import { storage } from "@lib/firebase";
+
+class _StorageService {
+  async uploadFile(file: Buffer, filename: string) {
+    if (file.length > 5 * 1024 * 1024) {
+      throw new Error("File size exceeds the limit of 5 MB.");
+    }
+
+    const storageRef = ref(storage, `avatars/${filename}`);
+    const { metadata } = await uploadBytes(storageRef, file);
+
+    if (!metadata.fullPath) {
+      throw new Error("There was some error while uploading the file.");
+    }
+
+    const fileURL = await getDownloadURL(storageRef);
+    return { fileURL };
+  }
+}
+
+export const StorageService = new _StorageService();
